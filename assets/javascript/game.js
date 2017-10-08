91344239703067bfda8d9473d86e71b4297a22db
Harry Spencer <haspencer4@gmail.com>
2017-10-08 12:15:17 -0500

    working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 var triesCounter = 15;
 var dictionary = ["picard", "crusher", "worf","data","enterprise", "starfleet", "federation", "klingon", "vulcan", "duras", "serak", "guinan", "gordi", "troi", "riker"];
+var guesses = []
+//possibly set all the variables at the top, since that is apparently better formed
 
 var word = getRandomWord();
 
@@ -52,9 +54,27 @@ document.onkeyup = function(event){
 			console.log("New word: " + word);
 		}
 		document.getElementById("counter").innerHTML = triesCounter;
+	if(guesses.indexOf(choice) === -1){
+			guesses.push(choice);
+			document.getElementById("guesses").innerHTML = guesses.join(" ");
+}
 	}
 
-var displayedAnswer = [];
+
+function getNewWord(){
+		guesses = [];
+		arrayWd = [];
+		dashesArr = [];	
+		word = getRandomWord();
+		arrayWd = splitWord(word);
+		triesCounter = arrayWd.length + 5;
+		document.getElementById("guesses").innerHTML = "Guesses Remaining: " + triesCounter;
+		dashesArr = createDashesArray(arrayWd, dashesArr);
+		document.getElementById("word").innerHTML = dashesArr.join(" ");	
+
+}
+
+/*var displayedAnswer = [];
 for (var i = 0; i < arrayWd.length; i++) {
 	if (arrayWd.charAt[i] !=' ') {
 		displayedAnswer.push('_');
@@ -62,7 +82,7 @@ for (var i = 0; i < arrayWd.length; i++) {
 	else{
 	displayedAnswer.push(' ');
 	}
-}
+}*/
 
 
 
